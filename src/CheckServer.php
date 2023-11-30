e466ef0613b69e3dcab42904bec2ddba8d8b4ef1
wuzhenlong <wuzhenlong@onemt.com.cn>
2023-11-30 14:14:24 +0800

    update

--- src/CheckServer.php
+++ src/CheckServer.php
@@ -49,13 +49,13 @@ class CheckServer
         socket_set_option($socket, SOL_SOCKET, SO_SNDTIMEO, ["sec"=>$this->socketSendTimeout, "usec"=>0]);
 
         if ($socket === false) {
-            throw new ConnectErrorException(socket_strerror(socket_last_error()));
+            throw new ConnectErrorException($this->host.':'.$this->port.' connect error:'.socket_strerror(socket_last_error()));
         } else {
             // 尝试连接到 MySQL 服务器
             $result = socket_connect($socket, $this->host, $this->port);
 
             if ($result === false) {
-                throw new ConnectErrorException(socket_strerror(socket_last_error()));
+                throw new ConnectErrorException($this->host.':'.$this->port.' connect error:'.socket_strerror(socket_last_error()));
             } else {
                 // 构建 MySQL 握手消息
                 $handshake = "\x0\x0\x0\x10\x4a\x4d\x20\x20\x20\x20\x20\x20\x00\x00\x00\x00\x001";
@@ -66,7 +66,7 @@ class CheckServer
                 $response = socket_read($socket, 1024);
 
                 if ($response === false){
-                    throw new ServerErrorException('The server response timed out or may not be a MySQL server.');
+                    throw new ServerErrorException('The server '.$this->host.':'.$this->port.' response timed out or may not be a MySQL server.');
                 }
                 // 关闭 socket 连接
                 socket_close($socket);
