d0d311d519e9babd7a5ccd360cb38c1402ca5597
wuzhenlong <wuzhenlong@onemt.com.cn>
2023-11-30 14:42:16 +0800

    update

--- src/CheckServer.php
+++ src/CheckServer.php
@@ -43,6 +43,16 @@ class CheckServer
         $this->checkServer->setSocketSendTimeout($socketSendTimeout);
     }
 
+    public function setHost($host)
+    {
+        $this->checkServer->setHost($host);
+    }
+
+    public function setPort($port)
+    {
+        $this->checkServer->setPort($port);
+    }
+
     public function check()
     {
         return $this->checkServer->check();
